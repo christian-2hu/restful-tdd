83ffbe068027a5c3f30adf4a36d2af00f253e18d
Christian <60179743+christian-2hu@users.noreply.github.com>
2023-03-03 17:12:09 -0300

    Initial commit

---
+++ src/test/java/com/SpringTdd/SpringTddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringTdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
