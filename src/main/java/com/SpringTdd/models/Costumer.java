1891156c193243010780ce2bccc2e894f09814d8
Christian <60179743+christian-2hu@users.noreply.github.com>
2023-03-03 17:21:18 -0300

    Add Costumer model
    Since it does only have getters and setters, there's no need to test it

---
+++ src/main/java/com/SpringTdd/models/Costumer.java
@@ -0,0 +1,34 @@
+package com.SpringTdd.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "costumer")
+@Getter
+@Setter
+public class Costumer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    private String email;
+
+    private String gender;
+
+    @Column(name = "credit_card")
+    private String creditCard;
+}
